4c3f9dd1f5062e5990a296dda6bb532f9c031b65
Sean Clifford <sclifford6262@gmail.com>
2018-06-04 19:10:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function showRepositories(event, data) {
 
 function showCommits(){
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(c => '<li><strong>' + c.author.login + '</strong> - ' + c.commit.message + '</li>').join('')})
-}
+  const commitsList = `<ul>${commits.map(c => '<li><strong>' + c.author.login + '</strong> - ' + c.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML += commitsList
+
 
 
 function getRepositories(){
