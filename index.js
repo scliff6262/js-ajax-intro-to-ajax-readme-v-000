dd97f52ce790b0e2ce7538e55208211a71ca2160
Sean Clifford <sclifford6262@gmail.com>
2018-06-04 18:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function showRepositories(event, data){
     repoList += "<li>" + this.responseText[i]["name"] + "</li>"
   }
   repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getRepositories(){
